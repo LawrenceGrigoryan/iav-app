b1e3a7b16db84dbca32252df530bb531f1d86490
LawrenceGrigoryan <glavrentiy123@gmail.com>
2024-08-26 21:54:28 +0400

    upd: finished joker service

---
+++ services/joker/joker.py
@@ -0,0 +1,52 @@
+import logging
+import os
+import traceback
+from contextlib import asynccontextmanager
+from pathlib import Path
+
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from omegaconf import OmegaConf
+
+from .model import GeminiModel
+from .utils import JokerRequest
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup events
+    # load env variables
+    env_path = Path(Path(__file__).resolve().parents[2], ".env")
+    load_dotenv(env_path)
+    GEMINI_API_KEY = os.environ.get("GEMINIT_API_KEY")
+    
+    # load model config
+    config_path = Path(Path(__file__).resolve().parents[0], "model_config.yaml")
+    model_config = OmegaConf.load(config_path)
+    
+    # init model and config once
+    app.state.model = GeminiModel(GEMINI_API_KEY, model=model_config.model)
+    app.state.model_config = model_config
+
+    yield
+    # shutdown
+    del app.state.model
+    del app.state.model_config
+    
+
+# init app
+app = FastAPI(docs_url=True)
+
+
+@app.post("/generate_joke")
+def generate_joke(joker_request: JokerRequest) -> str:
+    try:
+        prompt = joker_request.prompt
+        joke = app.state.model(prompt=prompt, generation_config=app.state.model_config.generation_config)
+    except Exception:
+        logging.info(traceback.format_exc())
+        joke = "Error occurred, try again"
+        
+    return joke
