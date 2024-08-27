88418d74476a56737829d8a621a94a3ee1ce8b4c
LawrenceGrigoryan <glavrentiy123@gmail.com>
2024-08-27 16:42:02 +0400

    fix: cv paths

--- services/streamlit_app/pages/Joker.py
+++ services/streamlit_app/pages/Joker.py
@@ -2,7 +2,11 @@ import os
 import streamlit as st
 import requests
 
+model_url = os.environ.get("MODEL_URL")
+
 st.markdown("# AI Joker 🤡")
 
 if st.button("Generate a joke"):
-     st.write('Generating a joke')
+     response_json = requests.post(model_url, json={"prompt": "generate a joke"}).json()
+     joke = response_json["response"]
+     st.write(joke)
